526148e17ec5380a3c63d1693a9807e7a5054cf5
mtbishop <bishopmatthew76@gmail.com>
2022-02-25 18:04:35 -0700

    zip search section for mobile and tablet

--- src/components/SearchZip.js
+++ src/components/SearchZip.js
@@ -1,10 +1,16 @@
 import '../style/SearchZip.scss';
+import ZipSearchBackground from '../style/img/background_image.jpg';
 
 const SearchZip = () => {
   return (
     <>
       <section className="container searchZip-container">
-        <p>SearchZip</p>
+        <div className="zipSearch-wrapper">
+          <img alt="dog-background" src={ZipSearchBackground} />
+          <div className="search-div">
+            <p>Enter your zip code to find dogs near you that need a home!</p>
+          </div>
+        </div>
       </section>
     </>
   );
